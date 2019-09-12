a78caddad1a55abe43ee835370772803dc358aa6
arunkumarsa7 <arunkumarsa8374@gmail.com>
2019-09-13 01:27:03 +0530

    code changes for the portfolio page

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -37,7 +38,7 @@ export class PortfolioComponent implements OnInit, AfterViewInit {
       .subscribe(params => {
         let id = params.get('id');
         let elem: HTMLElement = document.getElementById('appTitle');
-        elem.innerHTML = elem.innerHTML + ' | ' + id;
+        elem.innerHTML = AppComponent.title + ' | ' + id;
       });
   }
 
