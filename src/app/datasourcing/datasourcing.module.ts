cdf77085f05d46545531256fb82afdbe38189cd3
Ayushi <ayushisingh1002@gmail.com>
2019-09-24 15:42:36 +0530

    table sla changes

--- src/app/datasourcing/datasourcing.module.ts
+++ src/app/datasourcing/datasourcing.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DatasourcingComponent} from './datasourcing.component';
 import { ServiceDescriptionComponent } from './service-description/service-description.component';
 import { KeyContactsComponent } from './key-contacts/key-contacts.component';
+import { ServicesSLAComponent } from './services-sla/services-sla.component';
 
 @NgModule({
     declarations: [
       KeyContactsComponent,
         ServiceDescriptionComponent,
-        DatasourcingComponent
+        DatasourcingComponent,
+        ServicesSLAComponent
     ],
     imports: [
         CommonModule,
