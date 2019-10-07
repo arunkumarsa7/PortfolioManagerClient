68b1b58c3f471e86dc92ea57de654bf3288cbee4
sam13113 <sarath2madhu@gmail.com>
2019-10-07 15:17:08 +0530

    Hotfix: issue 49

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { PortfolioComponent } from './modules/portfolio/portfolio.component';
 import { AppConstants } from './shared/constant/app-constants';
 
 const routes: Routes = [
-  { path: AppConstants.dashboardPageUrl, loadChildren: () => {
-    return import('./modules/dashboard/dashboard.module').then(mod => mod.DashboardModule);
-  }},
+  { path: AppConstants.dashboardPageUrl, loadChildren:
+    './modules/dashboard/dashboard.module#DashboardModule'
+  },
   { path: AppConstants.projectsPageUrl, component: PortfolioComponent , data: {animation: AppConstants.projectsPageKey}},
   { path: AppConstants.portfolioPageUrl, component: PortfolioComponent , data: {animation: AppConstants.portfolioPageKey} },
   { path: AppConstants.homePageUrl, component: HomeComponent, data: {animation: AppConstants.homePageKey} },
