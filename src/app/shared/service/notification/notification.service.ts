e0af92058509225ca7abdf19be9e2ddb78141ac5
sam13113 <sarath2madhu@gmail.com>
2019-10-18 17:12:07 +0530

    Notification services for snackbar display with standard color palatte

--- src/app/shared/service/notification/notification.service.ts
+++ src/app/shared/service/notification/notification.service.ts
@@ -6,11 +6,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class NotificationService {
 
-  constructor(public snackBar: MatSnackBar, private zone: NgZone) { }
+  constructor(public snackBar: MatSnackBar, private zone: NgZone) {
+  
+   }
 
   showSuccess(message: string): void {
-    this.zone.run(() => {
-      this.snackBar.open(message);
+    this.zone.run(() => { 
+      this.snackBar.open(message,'',{panelClass: ['success-snackbar'], duration:5000});
     });
   }
 
@@ -18,7 +20,7 @@ export class NotificationService {
     this.zone.run(() => {
       // The second parameter is the text in the button.
       // In the third, we send in the css class for the snack bar.
-      this.snackBar.open(message, 'X', {panelClass: ['error']});
+      this.snackBar.open(message, 'X', {panelClass: ['error-snackbar'], duration:50000});
     });
 
   }
